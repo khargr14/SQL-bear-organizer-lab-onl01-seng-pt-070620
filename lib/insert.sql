028081e899569ea71e4996d3fee000790d30202f
Keesha Hargrove <kharg14@gmail.com>
2020-09-07 13:42:33 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,2 +1,2 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 5, "Male", "Brown", "Social", false);
-INSERT INTO bears (name, age, gender, temperament, alive) VALUES ('Maru', 3, 'Scottish Fold');
+INSERT INTO bears (name, age, gender, temperament, alive) VALUES ('Rowdy', 3, "Male", "Social", "true");
