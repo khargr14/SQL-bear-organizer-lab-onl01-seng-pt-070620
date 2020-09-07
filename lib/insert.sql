997de1b926ab55d3c415473538ee4555588ba2be
Keesha Hargrove <kharg14@gmail.com>
2020-09-07 13:53:16 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -3,6 +3,6 @@ INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy"
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 0, "Female", "Black", "Shy",1);  
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 12, "Male", "Brown", "Angry", 1);  
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 2, "Female", "Brown", "Shy", 0);  
-INSERT INTO bears (name, age, gender, color, temperament, a0live) VALUES ("Grinch", 6, "Female", "Light Brown", "Shy", 0);  
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 6, "Female", "Light Brown", "Shy", 0);  
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 3, "Female", "Brown", "Sleepy", 0);  
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, NULL, NULL, "Black", "EVIL", 0);  
