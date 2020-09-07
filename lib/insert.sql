59334bfb43db615fcae36c77154438a2e1e9d70f
Keesha Hargrove <kharg14@gmail.com>
2020-09-07 13:45:19 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,2 +1,8 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 5, "Male", "Brown", "Social", false);
-INSERT INTO bears (name, age, gender, temperament, alive) VALUES ('Rowdy', 3, "Male", "Social", "true");
+INSERT INTO bears (name, age, gender, temperament, alive) VALUES ('Rowdy', 3, "Male", "Social","Black" ,"true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 1 "Female", "Black", "Shy", true);  
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 10, "Male", "Brown", "Happy", true);  
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 2, "Female", "Black", "Shy", true);  
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 4, "Female", "Light Brown", "Shy", false);  
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 3, "Female", "Brown", "Boring", false);  
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, NULL, NULL, "Black", "Silk", true);  
